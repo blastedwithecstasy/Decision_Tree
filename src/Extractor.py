dabf72eb67e38286e434292f225ad5a1e0cca497
AdamG <trialbyrest@gmail.com>
2023-03-26 12:25:29 +1300

    Debugged the data extractor Extractor.py. Next step is to build and test all the functions that make a tree.

--- src/Extractor.py
+++ src/Extractor.py
@@ -1,54 +1,49 @@
 import pandas as pd
 
-# Read the data from CSV file and replace ? with NaN
-df = pd.read_csv('even_smaller_data.txt', header=None, na_values=['?'])
+## Read the data from CSV file and replace '?' with 'NaN'
+mushDF = pd.read_csv('even_smaller_data.txt', header=None, na_values=['?'])
 
-# Rename columns
-df.columns = ['Edible', 'Cap', 'Stalk','Solitary']
+## Rename columns
+mushDF.columns = ['Poisonous', 'Cap', 'Stalk','Solitary', 'Fake']
+print(mushDF)
 
-# Print the DataFrame
-print(df)
+## Delete the data points with missing values 
+mushDF = mushDF.dropna()
 
-# Drop the rows with 'NaN'
-df = df.dropna()
+def oneHotEncoder(df):
+    ## Find the attributes with more than two possible values 
+    columnsToEncode = []
+    for column in df.columns:
+        numUniqueVelue = df[column].nunique()
+        if numUniqueVelue > 2:
+            columnsToEncode.append(column)
+    ## get_dummies() one-hot encodes the relavent columns
+    df = pd.get_dummies(df, columns= columnsToEncode)
+    return df
 
-# Define the columns that have more than two possible values
-cols_to_encode = []
-for col in df.columns:
-    num_unique = df[col].nunique()
-    if num_unique > 2:
-        cols_to_encode.append(col)
+mushdf = oneHotEncoder(mushDF)
+print(mushDF)
 
-# Use pandas' get_dummies function to one-hot encode the columns
-df = pd.get_dummies(df, columns=cols_to_encode)
-
-# Print the encoded data frame
-print(df)
-
-def make_mapping_dict(df):
-    mapping_dict = {}
+## Replaces the letters with '1's and '0's
+def replaceWithBinary(df):
     for col in df.columns:
-        unique_vals = df[col].unique()
-        if set(unique_vals) == set([0, 1]):
+        uniqueVals = df[col].unique()
+        if set(uniqueVals) == set([0, 1]):
             # If both 1 and 0 are present, leave the column unchanged
             continue
-        if len(unique_vals) != 2:
+        if len(uniqueVals) != 2:
             # If there are more than two unique values, skip the column
             continue
-        val_dict = dict(zip(unique_vals, range(len(unique_vals))))
-        # Find the character that should be mapped to 1
-        one_char = max(val_dict, key=val_dict.get)
-        # Find the character that should be mapped to 0
-        zero_char = min(val_dict, key=val_dict.get)
-        mapping_dict[one_char] = 1
-        mapping_dict[zero_char] = 0
-    return mapping_dict
+        ## Create a dictionary mapping that maps the max letter to 1 and the min letter to 0
+        mappingDict = {max(uniqueVals): 1, min(uniqueVals): 0}
+        df[col] = df[col].replace(mappingDict)
+    return df
 
-# Replace characters with 0 and 1
-mapping_dict = make_mapping_dict(df)
-df = df.replace(mapping_dict)
+mushDF = replaceWithBinary(mushDF) 
 
-# Convert data type to integer
-df = df.astype(int)
+## Convert data type of the data frame to integer
+mushDF = mushDF.astype(int)
+print(mushDF)
 
-print(df)
+mushDF = mushDF.drop('Fake', axis=1)
+print(mushDF)
